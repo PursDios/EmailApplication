9dc58c3a322f14b692a8954b8534b4af9a85cd1a
Ryan Cooper <pursdios942@gmail.com>
2019-05-25 16:33:28 +0100

    made the password text field actually use password characters instead of displaying the password in plain text

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -79,6 +79,7 @@
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(210, 20);
             this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
             // 
             // FrmLogin
             // 
